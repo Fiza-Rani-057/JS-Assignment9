28b44ec774302c0b0771968cca3e71347a039975
Fiza-Rani-057 <fahadsaeed057@gmail.com>
2026-06-25 00:06:12 +0500

    Arrays and Loops

---
+++ script.js
@@ -0,0 +1,26 @@
+//   Question 1
+
+document.write( "<h1>" + "Question 1" + "<br>"  + "</h1>");
+
+// MultiDimensional Array
+
+var array = [[], [], []];
+
+document.write("Empt Multidimensional Array");
+
+// Question 2
+
+ document.write( "<h1>" + "Question 2" + "<br>"  + "</h1>");
+
+ var array2 = [
+    [0 , 1  , 2 ,3],
+    [1 , 0 , 1 , 2],
+    [2 , 1 , 0 , 1],
+ ];
+
+ document.write(array2[0] + "<br>");
+ document.write(array2[1] + "<br>");
+ document.write(array2[2] + "<br>");
+
+
+
